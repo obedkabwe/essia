a9a52d54848c81b7dc3f84e8ac292f020615006c
obedkabwe <kabweobed@gmail.com>
2022-05-25 16:56:09 -0500

    loca

---
+++ src/ContaBanco.java
@@ -0,0 +1,132 @@
+
+public class ContaBanco {
+
+	public Integer numConta;
+	protected String tipo;
+	private String dono;
+	private float saldo;
+	private boolean status;
+	
+	
+	
+	
+	public ContaBanco() {
+		
+	}
+
+
+
+
+	public Integer getNumConta() {
+		return numConta;
+	}
+
+
+
+
+	public void setNumConta(Integer numConta) {
+		this.numConta = numConta;
+	}
+ 
+
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+
+	public String getDono() {
+		return dono;
+	}
+
+
+
+
+	public void setDono(String dono) {
+		this.dono = dono;
+	}
+
+
+
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+
+
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+
+
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+	public void abrirConta() {
+		
+	}
+
+	public void fecharConta() {
+		
+	}
+
+	public void depositar() {
+		
+	}
+
+	public void sacar() {
+		
+	}
+
+	public void pagarMensal() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
